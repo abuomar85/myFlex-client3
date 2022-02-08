bcd1c6b617d4fb53ff8f5ec05f04d26c4836649c
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2022-02-09 00:16:51 +0100

    update index file and createing loginview
    creating loginview
    Replace the hardcoded movies array with the one fetched from myFlix API
    Add two new views: one for new users to register for myFlix and another for existing users to log in to the app
    Create a  link for unregistered users

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import axios from "axios";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export class MainView extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            movies: [
-              { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg', Genre: 'Action , Adventure ,sci' , Director: 'Christopher Nolan'},
-              { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg' , Genre: 'Drama' , Director: 'Frank Darabont'},
-              { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png' , Genre: 'Drama, Action, Adventure' , Director: 'Ridley Scott'}
-            ],
+            movies: [],
             selectedMovie: null
         }
+    }
+
+    componentDidMount() {
+      axios.get('https://limitless-sierra-99077.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -21,21 +31,27 @@ export class MainView extends React.Component {
             selectedMovie: newSelectedMovie
         })
     }
+
+    onLoggedIn(user) {
+      this.setState({
+        user
+      });
+    }
     render() {
-        const { movies, selectedMovie } = this.state;
-    
-    
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-    
-        return (
-          <div className="main-view">
-            {selectedMovie
-              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-              : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-              ))
-            }
-          </div>
-        );
+      const { movies, selectedMovie, user } = this.state;
+      if(!user) return<LoginView onLoggedIn={user=> this.onLoggedIn(user)}/>
+
+      if (movies.length === 0) return <div className="main-view" />;
+  
+      return (
+        <div className="main-view">
+          {selectedMovie
+            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            : movies.map(movie => (
+              <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+           ))
+          }
+        </div>
+      );
       }
 }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,39 +1,37 @@
 import React from "react";
 export class MovieView extends React.Component{
+
+    keypressCallback(event) {
+      console.log(event.key);
+    }
+
+    componentDidMount() {
+      document.addEventListener('keypress', this.keypressCallback);
+    }
+
+
     render()  {
-        const {movie, onBackClick} = this.props; 
+        const {movie,onBackClick} = this.props; 
 
         return (
-            <div className="movie-view">
+
+          <div className="movie-view">
             <div className="movie-poster">
-              <img src={movie.ImagePath} 
-                crossOrigin ="anonymous"
-              />
+              <img src={movie.ImagePath} crossOrigin ="anonymous" />
             </div>
+
             <div className="movie-title">
               <span className="label">Title: </span>
               <span className="value">{movie.Title}</span>
             </div>
+
             <div className="movie-description">
               <span className="label">Description: </span>
               <span className="value">{movie.Description}</span>
-
             </div>
-
-            <div className="movie-genre">
-              <span className="label">Genre: </span>
-              <span className="value">{movie.Genre}</span>
-
-            </div>
-
-            <div className="movie-director">
-              <span className="label">Director: </span>
-              <span className="value">{movie.Director}</span>
-
-            </div>
-            <button onClick={() => { onBackClick(null); }}>Back</button>
-
-          </div>
+            <button onClick={() => { onBackClick(null); }}>Back</button>          
+       </div>
+         
         )
     }
 }
